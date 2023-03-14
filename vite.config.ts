a571c4d70a92a39c03265a8bd08a2b974d90a83f
Roger <Worth22Focus3000@gmail.com>
2023-03-14 14:49:53 +0800

    feat: add src path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+// also don't forget to `npm i -D @types/node`, so __dirname won't complain
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3001,
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
 })
