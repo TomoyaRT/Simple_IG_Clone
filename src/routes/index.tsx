b64d53eee8edd31315c4dfed2cd4e1bc539467b3
Roger <Worth22Focus3000@gmail.com>
2023-03-16 16:28:41 +0800

    fix: add react router basename

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,17 +3,22 @@ import Home from '@/pages/home'
 import Following from '@/pages/following'
 import ErrorPage from '@/pages/error'
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Home />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/following',
+      element: <Following />,
+      errorElement: <ErrorPage />,
+    },
+  ],
   {
-    path: '/',
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/following',
-    element: <Following />,
-    errorElement: <ErrorPage />,
-  },
-])
+    basename: '/Simple_IG_Clone/',
+  }
+)
 
 export default router
