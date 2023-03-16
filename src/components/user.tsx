40fd9b811dc6553623cf7ac8176250973f1bcfc5
Roger <Worth22Focus3000@gmail.com>
2023-03-16 11:20:08 +0800

    feat: using memo to avoid unnecessary re-rendering

--- src/components/user.tsx
+++ src/components/user.tsx
@@ -1,5 +1,6 @@
 import { useAppDispatch } from '@/hooks/store'
 import { follow, unFollow } from '@/store/slices/friendSlice'
+import { memo } from 'react'
 
 type UserProps = {
   size?: 'medium' | 'small'
@@ -11,49 +12,51 @@ type UserProps = {
   id?: number
 }
 
-const User: React.FC<UserProps> = ({
-  size = 'small',
-  showFollow = false,
-  isFollowing = false,
-  account,
-  location,
-  avatar,
-  id,
-}) => {
-  const dispatch = useAppDispatch()
+const User: React.FC<UserProps> = memo(
+  ({
+    size = 'small',
+    showFollow = false,
+    isFollowing = false,
+    account,
+    location,
+    avatar,
+    id,
+  }) => {
+    const dispatch = useAppDispatch()
 
-  function followClickHandler() {
-    console.log(id)
-    if (id === undefined) return
-    isFollowing ? dispatch(unFollow(id)) : dispatch(follow(id))
-  }
+    function followClickHandler() {
+      console.log(id)
+      if (id === undefined) return
+      isFollowing ? dispatch(unFollow(id)) : dispatch(follow(id))
+    }
 
-  return (
-    <div className="flex h-[70px] items-center box-border px-4">
-      <div
-        className={`${
-          size === 'small' ? 'w-[40px] h-[40px]' : 'w-[60px] h-[60px]'
-        } overflow-hidden rounded-full bg-cover bg-center`}
-        style={{
-          backgroundImage: `url(${avatar})`,
-        }}
-      ></div>
-      <div className="ml-4">
-        <p className="font-bold text-sm">{account}</p>
-        <p className="text-gray-400 text-xs">{location}</p>
-      </div>
-      {showFollow && (
-        <p
+    return (
+      <div className="flex h-[70px] items-center box-border px-4">
+        <div
           className={`${
-            isFollowing ? 'text-gray-700' : 'text-blue-400'
-          } ml-auto text-xs font-bold cursor-pointer`}
-          onClick={followClickHandler}
-        >
-          {isFollowing ? 'FOLLOWING' : 'FOLLOW'}
-        </p>
-      )}
-    </div>
-  )
-}
+            size === 'small' ? 'w-[40px] h-[40px]' : 'w-[60px] h-[60px]'
+          } overflow-hidden rounded-full bg-cover bg-center`}
+          style={{
+            backgroundImage: `url(${avatar})`,
+          }}
+        ></div>
+        <div className="ml-4">
+          <p className="font-bold text-sm">{account}</p>
+          <p className="text-gray-400 text-xs">{location}</p>
+        </div>
+        {showFollow && (
+          <p
+            className={`${
+              isFollowing ? 'text-gray-700' : 'text-blue-400'
+            } ml-auto text-xs font-bold cursor-pointer`}
+            onClick={followClickHandler}
+          >
+            {isFollowing ? 'FOLLOWING' : 'FOLLOW'}
+          </p>
+        )}
+      </div>
+    )
+  }
+)
 
 export default User
